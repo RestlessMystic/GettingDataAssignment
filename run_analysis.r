f75196a41b571ccfd82c585942bebab15c974756
Govind Maheswaran <Govind.M@xe04.ey.com>
2014-09-22 02:17:06 +0530

    Working still Part 4

--- run_analysis.r
+++ run_analysis.r
@@ -37,7 +37,7 @@ collist <- c(mean, std, 562, 563)     # Combining mean, std, subject and y_train
 data <- dframe[,sort(collist)]            # Subsetting data to choose mean and std columns
 
 # Naming columns, just for my understanding
-col.n <- c(as.character(feat$V2[feat$V1 %in% collist]), "SubjectList", "TrainY")
+col.n <- c(as.character(feat$V2[feat$V1 %in% collist]), "SubjectList", "Activity")
 colnames(data) <- col.n
 
 
@@ -45,11 +45,24 @@ colnames(data) <- col.n
 ###################          PART 3        ####################
 ############# Attaching Descriptive Activity Names ############ 
 
-data$TrainY <- as.factor(data$TrainY)
-levels(data$TrainY) <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS" , "SITTING", "STANDING", "LAYING")
+data$Activity <- as.factor(data$Activity)
+levels(data$Activity) <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS" , "SITTING", "STANDING", "LAYING")
 
 ###################          PART 4        ####################
 ############# Attaching Descriptive Activity Names ############ 
 
-
-
+proc <- colnames(data)
+proc <- gsub("BodyBody", "Body", proc)
+proc <- gsub("-mean\\(\\)", " Mean ", proc)
+proc <- gsub("-meanFreq\\(\\)", " Mean Frequency ", proc)
+proc <- gsub("-std\\(\\)", " Standard Deviation ", proc)
+proc <- gsub("Acc", " Acceleration ", proc)
+proc <- gsub("Mag", " Magnitude ", proc)
+proc <- gsub("Gyro", " Gyroscopic ", proc)
+proc <- gsub("-X", "Along X axis", proc)
+proc <- gsub("-Y", "Along Y axis", proc)
+proc <- gsub("-Z", "Along Z axis", proc)
+proc <- gsub("^t", "Time Domain ", proc)
+proc <- gsub("^f", "Frequency Domain ", proc)
+proc <- gsub("[ ]+", " ", proc)
+colnames(data) <- proc
